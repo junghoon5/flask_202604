75921ba93e70aa8ce5cee4715aa404bdc41930c5
junghoon5 <silkjh9521@gmail.com>
2026-04-01 17:39:27 +0900

    20260401

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 import config
 
+
 db = SQLAlchemy()
 migrate = Migrate()
 
@@ -17,7 +18,10 @@ def create_app():   # 애플리케이션 팩토리
     from . import models
 
     # 블루프린트 등록
-    from .views import main_views
+    from .views import main_views, question_views, answer_views
+
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
 
     return app
