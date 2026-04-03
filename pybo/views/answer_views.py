b878ac4eb67c6c97d711629010e572715a80d8fe
junghoon5 <silkjh9521@gmail.com>
2026-04-03 17:52:50 +0900

    20260403flask

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,22 +1,27 @@
 from datetime import datetime
 
-from flask import Blueprint, request, redirect, url_for
+from flask import Blueprint, request, redirect, url_for, render_template
 
 from pybo import db
+from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=['post'])
 def create(question_id):
+    form = AnswerForm()
     # 답변 등록할 질문 DB 조회
-    question = Question.query.get(question_id)
+    question = Question.query.get_or_404(question_id)
     # 사용자가 입력한 form태그의 'content'속성의 값을 변수에 담음
-    content = request.form.get('content')
-    # Answer 객체를 생성
-    answer = Answer(content=content, created=datetime.now())
-    # 해당 질문에 답변 등록
-    question.answer_set.append(answer)
-    db.session.commit()
+    if form.validate_on_submit():
+        content = form.content.data
 
-    return redirect(url_for('question.detail', question_id=question_id))
+        # Answer 객체를 생성
+        answer = Answer(content=content, created=datetime.now())
+        # 해당 질문에 답변 등록
+        question.answer_set.append(answer)
+        db.session.commit()
+        return redirect(url_for('question.detail', question_id=question_id))
+
+    return render_template('question/question_detail.html', question=question, form=form)
